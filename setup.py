4dc90c56f36172a8da40f78812438aa84e9304d8
André van Delft <andrevandelft@outlook.com>
2021-04-29 16:11:22 +0200

    Remove versioning from setup

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='leidenmark',
-    version='0.1.27',
-    description = 'A markdown extension for converting Leiden+ epigraphic text to TEI Epidoc XML',
+    description='A markdown extension for converting Leiden+ epigraphic text to TEI Epidoc XML',
     packages=find_packages(),
     install_requires=['markdown', 'lxml>=4.5', 'regex', 'dh_utils'],
     keywords='Leiden+ Markdown TEI Epidoc XML HTML',
