3504a9cb7e1d5ec68c85f977633128d02b40932f
André van Delft <andrevandelft@outlook.com>
2021-07-28 15:29:15 +0200

    Fix test name and add GitLab test job

--- tests/brackets_test.py
+++ tests/brackets_test.py
@@ -1,6 +1,6 @@
 from leidenmark import leiden_plus
 
 
-def above_test():
+def test_above():
     assert leiden_plus('\\above/') == '\\above/'
     assert leiden_plus('<= \\above/ =>') == '<ab><add place="above">above</add></ab>'
