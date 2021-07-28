9ac4cd0b768fa0e411e9db04f966d909154f7498
André van Delft <andrevandelft@outlook.com>
2021-07-28 11:35:19 +0200

    0.1.32

--- leidenmark/__init__.py
+++ leidenmark/__init__.py
@@ -27,7 +27,7 @@ from .brackets import (
 from .foreign import RE_FOREIGN, ForeignProcessor
 from .misc import HetaProcessor, RE_HETA
 
-__version__ = '0.1.31'
+__version__ = '0.1.32'
 
 
 class LeidenPlus(Extension):
