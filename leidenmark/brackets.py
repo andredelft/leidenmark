9a38d3b64c10318fdb5dc0203fb0cf40317b4ecd
André van Delft <andrevandelft@outlook.com>
2021-07-28 16:07:11 +0200

    6. Add interlinear processor

--- leidenmark/brackets.py
+++ leidenmark/brackets.py
@@ -15,6 +15,7 @@ def format_bracket_re(opening_bracket, closing_bracket):
 RE_SUPPLIED = format_bracket_re('[', ']')
 RE_ERASURE = format_bracket_re('\u301a\u27e6', '\u301b\u27e7')
 RE_ABOVE = format_bracket_re('\\', '/')
+RE_INTERLINEAR = r"\|\|interlin:\s*([^\|\n]+?)\|\|"
 
 
 class BracketProcessor(InlineProcessor, ContextInlineMixin):
@@ -52,6 +53,14 @@ class AboveProcessor(BracketProcessor):
     attrs = {'place': 'above'}
 
 
+class InterlinearProcessor(InlineProcessor):
+
+    def handleMatch(self, m, data):
+        el = etree.Element('add', place='interlinear')
+        el.text = m.group(1)
+        return el, m.start(), m.end()
+
+
 def register_brackets(md):
     md.inlinePatterns.register(
         SuppliedProcessor(RE_SUPPLIED, md), 'supplied', 5
@@ -61,4 +70,7 @@ def register_brackets(md):
     )
     md.inlinePatterns.register(
         AboveProcessor(RE_ABOVE, md), 'above', 114
+    )
+    md.inlinePatterns.register(
+        InterlinearProcessor(RE_INTERLINEAR, md), 'interlinear', 113
     )
--- tests/brackets_test.py
+++ tests/brackets_test.py
@@ -4,3 +4,7 @@ from leidenmark import leiden_plus
 def test_above():
     assert leiden_plus('\\above/') == '\\above/'
     assert leiden_plus('<= \\above/ =>') == '<ab><add place="above">above</add></ab>'
+
+
+def test_interlinear():
+    assert leiden_plus('||interlin: some text||') == '<add place="interlinear">some text</add>'
